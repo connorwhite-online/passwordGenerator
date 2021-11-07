7717727fee3a57bb6ec59dcb8e9211a2a68e1d89
Connor White <91504681+connorwhite-online@users.noreply.github.com>
2021-11-06 21:00:10 -0700

    initial project upload

---
+++ password-generator/script.js
@@ -0,0 +1,32 @@
+// Assignment code here
+
+var prompt = window.prompt("What year were you born in?");
+
+var promptTwo = window.prompt("What was your first street name?")
+
+function passcode () {
+
+  var prompt = window.prompt("What year were you born in?");
+
+  var promptTwo = window.prompt("What was your first street name?")
+
+}
+
+// What is this?
+document.getElementById("password").innerHTML = passcode;
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
