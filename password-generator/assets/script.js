95cddb2357e7f18ea24cd10592836bc883c7d565
Connor White <connorwhitepdx@gmail.com>
2021-12-12 14:51:52 -0800

    element selectors added

--- password-generator/assets/script.js
+++ password-generator/assets/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+var length = document.getElementById('#length');
+var lowercase = document.getElementById('#lowercase');
+var uppercase = document.getElementById('#uppercase');
+var numerical = document.getElementById('#numerical');
+var symbols = document.getElementById('#symbols');
+
+var lowerCodes = arrayLowToHigh(97, 122);
+var upperCodes = arrayLowToHigh(65, 90);
+var numericalCodes = arrayLowToHigh(48, 57);
+var symbolCodes = arrayLowToHigh(33,47).concat(
+  arrayLowToHigh(58, 64)
+).concat(
+  arrayLowToHigh(91, 96)
+).concat(
+  arrayLowToHigh(123, 126)
+)
+
+
+var generatePassword = function() {
+  var includeLower = lowercase.checked
+  var includeUpper = uppercase.checked
+  var includeNumbers = numerical.checked
+  var includeSymbols = symbols.checked
+};
+
+function arrayLowToHigh(low, high) {
+  var array = []
+  for (let i = low; i<= high; i++) {
+    array.push(i)
+  }
+  return array
+};
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
