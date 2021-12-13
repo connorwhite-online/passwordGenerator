23ac4027c9b8dbe704e1d784a20f7d1fa5715686
Connor White <connorwhitepdx@gmail.com>
2021-12-12 16:14:28 -0800

    full theoretical functionality added, but still not working

--- password-generator/assets/script.js
+++ password-generator/assets/script.js
@@ -5,6 +5,8 @@ var lowercase = document.getElementById('#lowercase');
 var uppercase = document.getElementById('#uppercase');
 var numerical = document.getElementById('#numerical');
 var symbols = document.getElementById('#symbols');
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
 var lowerCodes = arrayLowToHigh(97, 122);
 var upperCodes = arrayLowToHigh(65, 90);
@@ -17,17 +19,19 @@ var symbolCodes = arrayLowToHigh(33,47).concat(
   arrayLowToHigh(123, 126)
 )
 
-
-var generatePassword = function() {
+generateBtn.addEventListener("click", writePassword => {
+  writePassword.preventDefault()
   var characterLength = length.value
   var includeLower = lowercase.checked
   var includeUpper = uppercase.checked
   var includeNumbers = numerical.checked
   var includeSymbols = symbols.checked
-};
+  var password = generatePassword(length, uppercase, numerical, symbols);
+  passwordText.value = password;
 
+})
 
-function createPassword(length, lowercase, uppercase, numerical, symbols) {
+function generatePassword(length, lowercase, uppercase, numerical, symbols) {
   var charCodes = lowerCodes
   if (upperCodes) charCodes = charCodes.concat(upperCodes)
   if (numericalCodes) charCodes = charCodes.concat(numericalCodes)
@@ -35,8 +39,7 @@ function createPassword(length, lowercase, uppercase, numerical, symbols) {
 
   var passwordCharacters = []
   for (let i = 0; i < length; i++) {
-    var character = charCodes[Math.floor(Math.random() *
-    length)]
+    var characterCode = charCodes[Math.floor(Math.random() * length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
@@ -52,17 +55,17 @@ function arrayLowToHigh(low, high) {
 };
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword(length, lowercase, uppercase, numerical, symbols);
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
