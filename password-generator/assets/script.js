a2c92555611179414e2945d5918ba7af2c8e3daa
Connor White <connorwhitepdx@gmail.com>
2021-12-12 20:33:52 -0800

    comments added, text area converted to h3

--- password-generator/assets/script.js
+++ password-generator/assets/script.js
@@ -1,58 +1,67 @@
-// Assignment code here
+// define element selectors
+const length = document.getElementById('length')
+// const lowercase = document.getElementById('lowercase')
+const uppercase = document.getElementById('uppercase')
+const numerical = document.getElementById('numerical')
+const symbols = document.getElementById('symbols')
+const form = document.getElementById('password-form')
+const passwordText = document.getElementById('password')
 
-var length = document.getElementById('#length');
-var lowercase = document.getElementById('#lowercase');
-var uppercase = document.getElementById('#uppercase');
-var numerical = document.getElementById('#numerical');
-var symbols = document.getElementById('#symbols');
-var passwordText = document.querySelector("#password");
-var generateBtn = document.querySelector("#generate");
-
-var lowerCodes = arrayLowToHigh(97, 122);
-var upperCodes = arrayLowToHigh(65, 90);
-var numericalCodes = arrayLowToHigh(48, 57);
-var symbolCodes = arrayLowToHigh(33,47).concat(
+// define charCode arrays
+const lowerCodes = arrayLowToHigh(97, 122)
+const upperCodes = arrayLowToHigh(65, 90)
+const numericalCodes = arrayLowToHigh(48, 57)
+const symbolCodes = arrayLowToHigh(33,47).concat(
   arrayLowToHigh(58, 64)
 ).concat(
   arrayLowToHigh(91, 96)
 ).concat(
   arrayLowToHigh(123, 126)
-)
+);
 
-generateBtn.addEventListener("click", writePassword => {
-  writePassword.preventDefault()
-  var characterLength = length.value
-  var includeLower = lowercase.checked
-  var includeUpper = uppercase.checked
-  var includeNumbers = numerical.checked
-  var includeSymbols = symbols.checked
-  var password = generatePassword(length, uppercase, numerical, symbols);
-  passwordText.value = password;
-
+// read checkbox status and prevent submit from refreshing page
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterLength = document.getElementById('length').value
+  const includeUpper = document.getElementById('uppercase').checked
+  const includeNumbers = document.getElementById('numerical').checked
+  const includeSymbols = document.getElementById('symbols').checked
+  const password = generatePassword(length, uppercase, numerical, symbols)
+  passwordText.innerHTML = password
 })
 
+// generate password based on checked criteria
 function generatePassword(length, lowercase, uppercase, numerical, symbols) {
-  var charCodes = lowerCodes
+  let charCodes = lowerCodes
   if (upperCodes) charCodes = charCodes.concat(upperCodes)
   if (numericalCodes) charCodes = charCodes.concat(numericalCodes)
   if (symbolCodes) charCodes = charCodes.concat(symbolCodes)
 
-  var passwordCharacters = []
+  const passwordCharacters = []
   for (let i = 0; i < length; i++) {
-    var characterCode = charCodes[Math.floor(Math.random() * length)]
+    const characterCode = charCodes[Math.floor(Math.random() * length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
 }
 
-
+// create range for charCodes
 function arrayLowToHigh(low, high) {
-  var array = []
+  const array = []
   for (let i = low; i<= high; i++) {
     array.push(i)
   }
   return array
-};
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  console.log(password)
+}
+
 
 
 // // Get references to the #generate element
