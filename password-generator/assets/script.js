da36f345348ca30522a921a7ff519690775a7473
Connor White <connorwhitepdx@gmail.com>
2021-12-13 21:44:27 -0800

    event listener added and checkbox values read

--- password-generator/assets/script.js
+++ password-generator/assets/script.js
@@ -5,12 +5,21 @@ const numbers = "0123456789";
 const symbols = "=+-_)(*&^%$#@!";
 
 // Add constants for DOM ID selectors
-const passwordTxt = document.getElementById("password");
+const passwordText = document.getElementById("password");
 const length = document.getElementById("length");
 const incUpper = document.getElementById("uppercase");
 const incNumbers = document.getElementById("numbers");
 const incSymbols = document.getElementById("symbols");
-const generateBtn = document.getElementById("generate");
+const generateButton = document.getElementById("generate");
+
+// Event added to button and checkbox input values read. Generate string and append it to input value
+generateButton.addEventListener("click", () => {
+  let characters = lowercase;
+  incUpper.checked ? (characters += uppercase) : "";
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordText.value = generatePassword(length.value, characters);
+});
 
 // Return an indexed character and round the randomized number to an integer and multiply it by the # input. 
 const generatePassword = (length, characters) => {
