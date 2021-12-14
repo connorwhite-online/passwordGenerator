4f33f89c343151fe6c22974aecf2b52ee24f4572
Connor White <connorwhitepdx@gmail.com>
2021-12-13 21:29:36 -0800

    constants for DOM ID selectors added

--- password-generator/assets/script.js
+++ password-generator/assets/script.js
@@ -1,7 +1,17 @@
+// Establish characters to display in the password
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789";
 const symbols = "=+-_)(*&^%$#@!";
+
+// Add constants for DOM ID selectors
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incUpper = document.getElementById("uppercase");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
+const generateBtn = document.getElementById("generate");
+
 
 
 // define element selectors
